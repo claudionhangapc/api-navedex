f0c370609a4e5400a6b789bc004794c751feeb6b
claudionhangapc <claudionhangapc@gmail.com>
2021-11-10 00:49:19 -0300

    knex and .env config

--- knexfile.js
+++ knexfile.js
@@ -1,13 +1,16 @@
 const path = require("path");
+require('dotenv').config();
+
+//console.dir(process.env.POSTGRES_DB);
 
 module.exports = {
 
   development: {
     client: 'pg',
     connection: {
-      database:'navedex',
-      user:'postgres',
-      password:'root1234'
+      database:process.env.POSTGRES_DB,
+      user:process.env.POSTGRES_USER,
+      password:process.env.POSTGRES_PASSWORD
     },
     migrations:{
       tableName:'knex_migrations',
