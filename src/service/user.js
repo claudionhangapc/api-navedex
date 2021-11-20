d7dcf1e0fd17762628f54a21fe4f362fe90d1bd7
claudionhangapc <claudionhangapc@gmail.com>
2021-11-20 18:37:10 -0300

    gerando hash com bcrypt

--- src/service/user.js
+++ src/service/user.js
@@ -1,3 +1,5 @@
+const PASSWORD_SALT = 10
+
 class User{
 
   constructor(fastify){
@@ -11,19 +13,24 @@ class User{
 
   async siginup(email,password){
     
-    let result 
-
     try{
-       await this.model.insert({
+
+      const bcrypt = require('bcrypt')
+
+      const user =  await this.model.insert({
         email,
-        password
+        password:bcrypt.hashSync(password, PASSWORD_SALT)
       })
-      result = true
+
+      return true
+
     }catch(error){
-       result = error
+
+      return error
+
     }
     
-    return result
+    
   }
 
    /*
@@ -32,21 +39,22 @@ class User{
 
   async login(email,password){
     try{
-      const result = await this.model.where({email,password})
-      if(result.length>0){
 
-        const {id, email} = result[0]
+      const bcrypt = require('bcrypt')
+      const user = await this.model.where({email})
+
+      if((user.length>0)&&(bcrypt.compareSync(password, user[0].password))){
+
+        const {id, email} = user[0]
         const payload = {id, email}
         const token = this.jwt.sign(payload)
         return {id,email, token}
 
       }else{
         return {
-
           statusCode: 400,
           error: "erro do cliente",
           message: "usuário não encontrado"
-
         }
       }
     }catch(error){
